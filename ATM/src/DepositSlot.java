e6a25f9bec56974ebc460910419cb96e959b867d
Victor Fantucci <victor.v.fantucci@gmail.com>
2021-03-09 14:20:22 -0300

    Create a new class [DepositSlot]

---
+++ ATM/src/DepositSlot.java
@@ -0,0 +1,12 @@
+// DepositSlot.java 
+// Represents the deposit slot of the ATM
+
+public class DepositSlot 
+{
+    // indicates whether the envelope was received
+    // (always returns true, because this is only a software simulation of a real deposit slot)
+    public boolean isEnvelopeReceived()
+    {
+        return true;
+    }// end method isEnvelopeReceived
+}// end class DepositSlot
