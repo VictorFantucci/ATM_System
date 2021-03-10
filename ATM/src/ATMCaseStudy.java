5a4d1edb38f8d3d3a017bcff7eb19ce32e943a7c
Victor Fantucci <victor.v.fantucci@gmail.com>
2021-03-09 22:46:03 -0300

    Create a new class [ATMCaseStudy]

---
+++ ATM/src/ATMCaseStudy.java
@@ -0,0 +1,12 @@
+// ATMCaseStudy.java
+// Driver program for the the ATM case study
+
+public class ATMCaseStudy
+{
+    // main method creates and runs the ATM
+    public static void main(String[] args)
+    {
+        ATM theATM = new ATM();
+        theATM.run();
+    }// end main
+}// end class ATMCaseStudy
